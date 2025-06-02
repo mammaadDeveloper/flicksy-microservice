9c074fa20bbf78cbfe0cc56375f4114a606ca9be
Mohammadreza <developerMammad@proton.me>
2025-06-02 11:23:54 +0330

    fix(user service, users modules): fixed the problem of adding a user during registration
    Change the Add User command handler.

--- apps/user-service/src/modules/users/users.module.ts
+++ apps/user-service/src/modules/users/users.module.ts
@@ -3,11 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './entities/user.entity';
 import { UsersService } from './users.service';
 import { UserRepositoryService } from './services/repository.service';
-import { CreateUserCommand } from './commands/create/create.command';
 import { FindUserQueryHandler } from './queries/find/find.handler';
 import { FindUserByEmailQueryHandler } from './queries/find-by-email/find-by-email.handler';
+import { CreateUserCommandHandler } from './commands';
 
-const commands = [CreateUserCommand];
+const commands = [CreateUserCommandHandler];
 const queries = [FindUserQueryHandler, FindUserByEmailQueryHandler];
 @Module({
     imports: [
