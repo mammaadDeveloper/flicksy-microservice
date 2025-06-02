156bc6bb37a761249ef8ff3548c9214c3e223170
Mohammadreza <developerMammad@proton.me>
2025-06-02 11:18:50 +0330

    feat(user service, filters): error filter not found

--- apps/user-service/src/main.ts
+++ apps/user-service/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { VersioningType } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { NotFoundExceptionFilter } from './common/filters/not-found-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,7 +14,11 @@ async function bootstrap() {
     type: VersioningType.URI,
   });
 
+  // CORS
   app.enableCors();
+
+  // Filters
+  app.useGlobalFilters(new NotFoundExceptionFilter())
 
   await app.listen(config.get('app.port'));
 }
