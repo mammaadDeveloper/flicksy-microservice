dc913284d4ea66298d0aba22b1d1fde774a02496
Mohammadreza <developerMammad@proton.me>
2025-06-08 23:54:08 +0330

    refactor(user service, response): minor changes to the response function

--- apps/user-service/src/shared/functions/response.fun.ts
+++ apps/user-service/src/shared/functions/response.fun.ts
@@ -5,53 +5,13 @@ export type ResourceObject<Attributes = any, Relationships = any> = {
   relationships?: Relationships;
 }
 export interface ResponsePayload {
-  //   statusCode: number;
   status?: string;
-  //   success: boolean;
   message: string;
-  data: ResourceObject | ResourceObject[];
+  data?: ResourceObject | ResourceObject[];
   meta?: Record<string, any>;
   links?: Record<string, any>;
 }
 
-export function validateResourceObject(obj: any): asserts obj is ResourceObject {
-  if (typeof obj !== 'object' || obj === null) {
-    throw new Error("Invalid resource object: value is not an object");
-  }
-
-  if (typeof obj.type !== 'string' || obj.type.trim() === '') {
-    throw new Error("Invalid resource object: 'type' must be a non-empty string");
-  }
-
-  if (!('attributes' in obj)) {
-    throw new Error("Invalid resource object: 'attributes' is required");
-  }
-
-  if (typeof obj.attributes !== 'object' || obj.attributes === null) {
-    throw new Error("Invalid resource object: 'attributes' must be an object or array");
-  }
-
-  if ('id' in obj && typeof obj.id !== 'string' && typeof obj.id !== 'number') {
-    throw new Error("Invalid resource object: 'id' must be a string or number if provided");
-  }
-
-  if (
-    'relationships' in obj &&
-    typeof obj.relationships !== 'object' &&
-    !Array.isArray(obj.relationships)
-  ) {
-    throw new Error("Invalid resource object: 'relationships' must be an object or array if provided");
-  }
-}
-
-
-
 export function response(payload: ResponsePayload) {
-  // if (Array.isArray(payload.data)) {
-  //   payload.data.forEach(validateResourceObject);
-  // } else {
-  //   validateResourceObject(payload.data);
-  // }
-
   return { __formatted: true, ...payload };
 }
