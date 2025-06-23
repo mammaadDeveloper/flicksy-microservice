480cde5da1df253ac2ca8db082422b1ac346cbf3
Mohammadreza <developerMammad@proton.me>
2025-06-23 18:22:53 +0330

    feat(movie service): customizing video information in response

---
+++ apps/movie-service/src/modules/movies/dto/response.dto.ts
@@ -0,0 +1,42 @@
+import { Expose, Type } from 'class-transformer';
+import { PosterEntity } from 'src/modules/posters/entities/posters.entity';
+import { SourceEntity } from 'src/modules/sources/entities/sources.entity';
+import { TrailerEntity } from 'src/modules/trailers/entities/trailers.entity';
+
+export class MovieResponseDto {
+  @Expose({name: 'slug'})
+  id: string;
+
+  @Expose()
+  title: string;
+
+  @Expose()
+  summary: string;
+
+  @Expose()
+  description?: string;
+
+  @Expose()
+  director: string;
+
+  @Expose()
+  year: number;
+
+  @Expose({ name: 'releaseDate' })
+  released_date?: Date;
+
+  @Expose({name: 'createdAt'})
+  created_at: Date;
+
+  @Expose()
+  @Type(() => PosterEntity)
+  posters?: PosterEntity[];
+
+  @Expose()
+  @Type(() => SourceEntity)
+  sources?: SourceEntity[];
+
+  @Expose()
+  @Type(() => TrailerEntity)
+  trailers?: TrailerEntity[];
+}
--- apps/movie-service/src/modules/movies/services/core.ts
+++ apps/movie-service/src/modules/movies/services/core.ts
@@ -2,25 +2,31 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { MoviesRepository } from './repository';
 import { GetMoviesWithPaginateDto } from '../dto/movies.dto';
 import { MovieEntity } from '../entities/movies.entity';
+import { plainToInstance } from 'class-transformer';
+import { MovieResponseDto } from '../dto/response.dto';
 
 @Injectable()
 export class CoreMoviesService {
   constructor(private readonly repository: MoviesRepository) {}
 
-  async findAll(options: GetMoviesWithPaginateDto): Promise<MovieEntity[]> {
-    return await this.repository.lookupMoviesWithPaginate(options);
+  async findAll(options: GetMoviesWithPaginateDto): Promise<unknown> {
+    const movies = await this.repository.lookupMoviesWithPaginate(options);
+    return plainToInstance(MovieResponseDto, movies, {
+      excludeExtraneousValues: true,
+    });
   }
 
-  async count(): Promise<number>{
+  async count(): Promise<number> {
     return await this.repository.count();
   }
 
-  async findOne(slug: string){
+  async findOne(slug: string) {
     const movie = await this.repository.findBySlug(slug);
 
-    if(!movie)
-      throw new NotFoundException('Movie not found');
+    if (!movie) throw new NotFoundException('Movie not found');
 
-    return movie;
+    return plainToInstance(MovieResponseDto, movie, {
+      excludeExtraneousValues: true,
+    });
   }
 }
