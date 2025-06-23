cfbecd2b4bebcad97723457a8924942723060fac
Mohammadreza <developerMammad@proton.me>
2025-06-23 17:29:58 +0330

    feat(movie service): preparing the endpoint to receive the list of movies

--- apps/movie-service/src/modules/movies/movies.controller.ts
+++ apps/movie-service/src/modules/movies/movies.controller.ts
@@ -1,19 +1,49 @@
-import { Controller, Get, Param, ParseUUIDPipe, Query } from "@nestjs/common";
-import { CoreMoviesService } from "./services/core";
-import { PaginateDto } from "src/common";
+import {
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseUUIDPipe,
+  Query,
+} from '@nestjs/common';
+import { CoreMoviesService } from './services/core';
+import { GetMoviesWithPaginateDto } from './dto/movies.dto';
+import { response } from 'src/shared';
 
 @Controller({
-    version: '1',
-    path: 'movies'
+  version: '1',
+  path: 'movies',
 })
-export class MoviesController{
-    constructor(
-        private readonly service: CoreMoviesService
-    ){}
+export class MoviesController {
+  constructor(private readonly service: CoreMoviesService) {}
 
-    @Get()
-    async findAll(@Query() options: PaginateDto){}
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  async findAll(@Query() options: GetMoviesWithPaginateDto) {
+    const page = options.page ?? 1;
+    const limit = options.limit ?? 10;
 
-    @Get(':slub')
-    async find(@Param('slug', new ParseUUIDPipe({version: '4'})) slug: string){}
+    const movies = await this.service.findAll({ ...options, page, limit });
+
+    return response({
+      message: 'Movie list information was successfully received.',
+      data: { type: 'movies', attributes: movies },
+      meta: {
+        currentPage: page,
+        itemCount: limit,
+        totalItems: 0,
+      },
+      links: {
+        self: `/movies?page=${options.page}`,
+        next: `/movies?page=${options.page + 1}`,
+        perv: `/movies?page=${options.page - 1}`,
+      },
+    });
+  }
+
+  @Get(':slub')
+  async find(
+    @Param('slug', new ParseUUIDPipe({ version: '4' })) slug: string,
+  ) {}
 }
